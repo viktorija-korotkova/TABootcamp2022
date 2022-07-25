c7a001914f08848ffe540473f5a5ef52d5663541
viktorija-korotkova <korotko.viktorija@gmail.com>
2022-07-25 17:26:25 +0300

    NumToWords

---
+++ src/com/bootcampexcercise/module2/activity/NumToWords.java
@@ -0,0 +1,41 @@
+package com.bootcampexcercise.module2.activity;
+
+public class NumToWords {
+        public static void main(String[] args) {
+            int numToWord;
+            numToWord = 5;
+            switch (numToWord)  {
+                case 1:
+                    System.out.println("One");
+                    break;
+                case 2:
+                    System.out.println("Two");
+                    break;
+                case 3:
+                    System.out.println("Three");
+                    break;
+                case 4:
+                    System.out.println("Four");
+                    break;
+                case 5:
+                    System.out.println("Five");
+                    break;
+                case 6:
+                    System.out.println("Six");
+                    break;
+                case 7:
+                    System.out.println("Seven");
+                    break;
+                case 8:
+                    System.out.println("Eight");
+                    break;
+                case 9:
+                    System.out.println("Nine");
+                    break;
+                case 10:
+                    System.out.println("Ten");
+                    break;
+            }
+
+        }
+}
