f67d6b836d06dc1aed427b5b98585880f3a4f369
viktorija-korotkova <korotko.viktorija@gmail.com>
2022-07-25 16:35:50 +0300

    Complete OperatorActivity

---
+++ src/com/bootcampexcercise/module2/sample/OperatorActivity.java
@@ -0,0 +1,14 @@
+package com.bootcampexcercise.module2.sample;
+
+public class OperatorActivity {
+    public static void main(String[] args) {
+        int myNum1;
+        int myNum2;
+        int result;
+        myNum1 = 10;
+        myNum2 = 15;
+        result = myNum1 + myNum2;
+        System.out.println("The sum of myNum1 and myNum2 is = " + result);
+    }
+
+}
