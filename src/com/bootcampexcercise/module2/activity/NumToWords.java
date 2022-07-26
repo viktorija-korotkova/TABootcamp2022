258e89fc5faf5deeff477d1944063a083480a63c
viktorija-korotkova <korotko.viktorija@gmail.com>
2022-07-26 11:54:16 +0300

    Find largest in array with method

--- src/com/bootcampexcercise/module2/activity/NumToWords.java
+++ src/com/bootcampexcercise/module2/activity/NumToWords.java
@@ -1,41 +1,46 @@
 package com.bootcampexcercise.module2.activity;
 
 public class NumToWords {
-        public static void main(String[] args) {
-            int numToWord;
-            numToWord = 5;
-            switch (numToWord)  {
-                case 1:
-                    System.out.println("One");
-                    break;
-                case 2:
-                    System.out.println("Two");
-                    break;
-                case 3:
-                    System.out.println("Three");
-                    break;
-                case 4:
-                    System.out.println("Four");
-                    break;
-                case 5:
-                    System.out.println("Five");
-                    break;
-                case 6:
-                    System.out.println("Six");
-                    break;
-                case 7:
-                    System.out.println("Seven");
-                    break;
-                case 8:
-                    System.out.println("Eight");
-                    break;
-                case 9:
-                    System.out.println("Nine");
-                    break;
-                case 10:
-                    System.out.println("Ten");
-                    break;
-            }
-
+    public static void main(String args[]) {
+        for(int i = 1; i <= 10; i++) {
+            numToWordsLogic(i);
         }
+    }
+    public static void numToWordsLogic(int x) {
+        switch(x) {
+            case 1:
+                System.out.println(x);
+                break;
+            case 2:
+                System.out.println(x);
+                break;
+            case 3:
+                System.out.println(x);
+                break;
+            case 4:
+                System.out.println(x);
+                break;
+            case 5:
+                System.out.println(x);
+                break;
+            case 6:
+                System.out.println(x);
+                break;
+            case 7:
+                System.out.println(x);
+                break;
+            case 8:
+                System.out.println(x);
+                break;
+            case 9:
+                System.out.println(x);
+                break;
+            case 10:
+                System.out.println(x);
+                break;
+            default:
+                System.out.println("Error");
+        }
+
+    }
 }
