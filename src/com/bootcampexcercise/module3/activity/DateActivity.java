5b4353d9cac098c0f209d3612568e3da2765a2cc
viktorija-korotkova <korotko.viktorija@gmail.com>
2022-07-26 22:33:49 +0300

    add DateActivity.java

---
+++ src/com/bootcampexcercise/module3/activity/DateActivity.java
@@ -0,0 +1,13 @@
+package com.bootcampexcercise.module3.activity;
+
+import java.util.Date;
+
+public class DateActivity {
+    public static void main(String args[]) {
+        // Create instance of Date Class- Date is already  provided by Java
+        // print the date
+
+        Date todayDate = new Date();
+        System.out.println(todayDate);
+    }
+}
