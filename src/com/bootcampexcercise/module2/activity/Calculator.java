95c46b6e4c1773ebc216e30231a494d5ca55e97d
viktorija-korotkova <korotko.viktorija@gmail.com>
2022-07-26 22:11:28 +0300

    Calculator.java implementation

--- src/com/bootcampexcercise/module2/activity/Calculator.java
+++ src/com/bootcampexcercise/module2/activity/Calculator.java
@@ -1,7 +1,11 @@
 package com.bootcampexcercise.module2.activity;
 
-public class CalculatorMethods {
+public class Calculator {
     public static void main(String[] args) {
+        startCalculations();
+    }
+
+    public static void startCalculations() {
         int x = 10;
         int y = 5;
         int[] nums = {1, 2, 3, 4};
@@ -11,38 +15,31 @@ public class CalculatorMethods {
         System.out.println("Divide - " + divide(x, y));
     }
 
-    private static int add(int x, int y) {
-        int sum = x + y;
-        return sum;
+    public static int add(int x, int y) {
+        return x + y;
     }
 
     private static int subtract(int x, int y) {
-        int substr = x - y;
-        return substr;
-        //TODO implement subtraction
+        return x - y;
     }
 
     private static int multiply(int[] numbers) {
-        int temp = 1;
+        int tempNum = 1;
         for (int i = 0; i < numbers.length; i++) {
-            temp = temp * numbers[i];
+            tempNum = tempNum * numbers[i];
         }
-        return temp;
+        return tempNum;
     }
 
     private static int divide(int x, int y) {
         int divValue = 0;
         if (x == 0 && y == 0) {
             System.out.println("dividing 0 with 0 is not possible");
-            //TODO complete the code so message is printed that dividing 0 with 0 is not possible
         } else if (y == 0) {
             System.out.println("division by 0 is not possible");
-            //TODO complete the code so message is printed that division by 0 is not possible
         } else {
             System.out.println("division is done");
-            //TODO complete the code so that division is done
         }
         return divValue;
     }
-
 }
