561ff297db33aa0c9feb2d2b4990be30ca4418e0
viktorija-korotkova <korotko.viktorija@gmail.com>
2022-07-26 10:07:53 +0300

    WhileLoop

---
+++ src/com/bootcampexcercise/module2/activity/WileLoop.java
@@ -0,0 +1,16 @@
+package com.bootcampexcercise.module2.activity;
+
+public class WileLoop {
+    public static void main(String[] args) {
+        // Print all even numbers less than 100
+        int i = 0;
+        while (i < 100) {
+            System.out.println("The odd number less than 100 is " + i);
+            i = i + 2;
+
+            //TODO: write code to Print all odd numbers less than 100
+        }
+
+    }
+}
+
