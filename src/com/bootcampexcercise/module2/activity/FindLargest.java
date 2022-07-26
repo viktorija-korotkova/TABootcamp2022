7196d842a33af84f82f2497a6e944af18f3c2198
viktorija-korotkova <korotko.viktorija@gmail.com>
2022-07-26 22:18:09 +0300

    add FindLargest.java

---
+++ src/com/bootcampexcercise/module2/activity/FindLargest.java
@@ -0,0 +1,14 @@
+package com.bootcampexcercise.module2.activity;
+
+public class FindLargest {
+    public static void main(String args[]) {
+        int x = 10;
+        int y = 88;
+
+        if (x > y) {
+            System.out.println("x is largest");
+        } else {
+            System.out.println("y is largest");
+        }
+    }
+}
