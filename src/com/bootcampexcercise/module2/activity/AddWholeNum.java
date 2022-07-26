45a4e0b7b6ae7345b48702e3491b3a87ab98a15c
viktorija-korotkova <korotko.viktorija@gmail.com>
2022-07-26 09:28:39 +0300

    AddWholeNum

---
+++ src/com/bootcampexcercise/module2/activity/AddWholeNum.java
@@ -0,0 +1,10 @@
+package com.bootcampexcercise.module2.activity;
+
+public class AddWholeNum {
+    public static void main(String[] args) {
+        for (int i = 50; i < 101; i++) {
+            System.out.println(i);
+        }
+}
+}
+
