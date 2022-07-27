05d8ca6cf121f8f07a4527c3f8812921e8dd457b
viktorija-korotkova <korotko.viktorija@gmail.com>
2022-07-27 17:04:15 +0300

    add module 6 activities

---
+++ src/com/bootcampexcercise/module6/activity/Cat.java
@@ -0,0 +1,16 @@
+package com.bootcampexcercise.module6.activity;
+
+//TODO: Cat class should implement Animal
+public class Cat implements Animal  {
+    //TODO: Create method animalSound() and its implementation should print out string "Cat says: 'Meow Meow'!"
+    @Override
+    public void animalSound() {
+        System.out.println("Cat says: 'Meow Meow'!");
+    }
+
+    //TODO: Create method sleep() and its implementation should print out string "Cat is sleeping!"
+    @Override
+    public void sleep() {
+        System.out.println("Cat is sleeping!");
+    }
+}
---
+++ src/com/bootcampexcercise/module6/activity/Mammal.java
@@ -0,0 +1,28 @@
+package com.bootcampexcercise.module6.activity;
+
+public class Mammal {
+
+    private Animal myAnimal;
+    private String name;
+
+    //TODO create constructor for Mammal with parameter name
+    public Mammal(String name) {
+        this.name = name;
+    }
+
+    public void setAnimal(Animal animal) {
+        myAnimal = animal;
+    }
+
+    public void animalIsSleeping() {
+        System.out.println("Animal with name: " + name+ " is ...");
+        //TODO: write code so that Animal sleep implementation is used
+        this.myAnimal.sleep();
+    }
+
+    public void animalIsSpeaking() {
+        System.out.println("Animal with name: " + name+ " is ...");
+       //TODO: write code so that Animal animalSound implementation is used
+        this.myAnimal.animalSound();
+    }
+}
