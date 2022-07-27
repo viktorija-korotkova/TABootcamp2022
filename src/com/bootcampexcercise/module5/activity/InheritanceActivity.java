935412aa75fc778602a94edaf495354b30866f06
viktorija-korotkova <korotko.viktorija@gmail.com>
2022-07-27 11:02:18 +0300

    add module5 activity

---
+++ src/com/bootcampexcercise/module5/activity/InheritanceActivity.java
@@ -0,0 +1,39 @@
+package com.bootcampexcercise.module5.activity;
+
+public class InheritanceActivity {
+
+	public static void main(String[] args) {
+		
+		// Create Person object p. Write code to and set name of Person as Sarah Johnson
+		// and age as 21
+		Person_I p = new Person_I("Sarah Johnson", 21);
+		
+		
+		System.out.println("-----------------------------");
+		//Create Employee object e and 
+		// set salary as 70000, title as Developer, age as 32 and name as Shawn Cun
+		Employee_I e = new Employee_I();
+		e.setTitle("Developer");
+		e.setAge(32);
+		e.setName("Shawn Cun");
+		e.setSalary(70000);
+		
+		
+		// Print Info using Employee object
+		System.out.println("-----------------------------");
+		System.out.println("Employee's Name is   : " + e.getName());
+		System.out.println("Employee's Age is    : " + e.getAge());
+		System.out.println("Employee's Title is  : " + e.getTitle());
+		System.out.println("Employee's Salary is : " + e.getSalary());
+		
+		
+		// Print Info using Person object
+		System.out.println("-----------------------------");
+		System.out.println("Person's Name is    : " + p.getName());
+		System.out.println("Person's Age is     : " + p.getAge());
+		
+			
+		
+	}
+
+}
---
+++ src/com/bootcampexcercise/module5/activity/Shape.java
@@ -0,0 +1,20 @@
+package com.bootcampexcercise.module5.activity;
+
+//TODO: change Shape class to abstract
+public abstract class Shape {
+
+	public String color;
+	
+	public Shape() {
+	}
+	
+	//TODO:  Add abstract method calculateArea() and calculatePerimeter() with return type as double
+	public abstract double calculateArea();
+	
+	public void setColor(String c) {
+		color = c;
+	}
+	public String getColor() {
+		return color;
+	}
+}
